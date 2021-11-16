f8b9cac7ff5adce3db523238054e7e9bfbd2beb8
SW2125\dltjd <leehj050211@gmail.com>
2021-11-16 17:54:04 +0900

    lcd에 거리 표시

--- smart_ruler.ino
+++ smart_ruler.ino
@@ -1,9 +1,14 @@
+#include <LiquidCrystal_I2C.h>
 #include "Adafruit_VL53L1X.h"
 #define IRQ_PIN 2
 #define XSHUT_PIN 3
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
   Serial.begin(115200);
   Wire.begin();
   if(!vl53.begin(0x29, &Wire)) {
@@ -19,11 +24,12 @@ void loop() {
   int distance;
   if(vl53.dataReady()){
     distance = vl53.distance();
-    if (distance==-1) {
-      Serial.print("Error");
+    lcd.clear();
+    if(distance==-1) {
+      lcd.print("error");
     }
-    Serial.print(distance);
-    Serial.println(" mm");
+    lcd.print(((String)distance)+"mm");
     vl53.clearInterrupt();
+    delay(1000);
   }
 }
